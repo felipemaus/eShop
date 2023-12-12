d27bf14b954f1d95c9273a99f74e41ce3d0c0d49
Felipe Flores Maus <felipe.maus@paipe.co>
2023-12-12 00:43:56 -0300

    feat: API-creation-and-register-user-integration

--- app/register/RegisterForm.tsx
+++ app/register/RegisterForm.tsx
@@ -7,6 +7,10 @@ import { FieldValues, useForm, SubmitHandler } from 'react-hook-form';
 import Button from "../components/Button";
 import Link from "next/link";
 import { AiOutlineGoogle } from "react-icons/ai";
+import axios from "axios";
+import toast from "react-hot-toast";
+import {signIn} from 'next-auth/react'
+import { useRouter } from "next/navigation";
 
 
 const RegisterForm=()=> {
@@ -20,9 +24,29 @@ const RegisterForm=()=> {
         }
     })
 
+    const router = useRouter()
+
     const onSubmit:SubmitHandler<FieldValues> = (data) => {
         setIsLoading(true);
-
+        axios.post("/api/register", data).then(()=>{
+            toast.success("Conta criada com sucesso!")
+            signIn('credentials', {
+                email:data.email,
+                password: data.password,
+                redirect:false
+            }).then((callback) => {
+                if (callback?.ok) {
+                    router.push('/cart');
+                    router.refresh();
+                    toast.success('Você está logado!');
+                }
+                if (callback?.error) {
+                    toast.error(callback.error)
+                }
+            })
+        }).catch(()=>{
+            toast.error("Algo está errado!");
+        }).finally(()=> setIsLoading(false));
          
     }
 
